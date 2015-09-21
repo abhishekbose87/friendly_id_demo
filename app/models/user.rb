3a549680e5f99015f0a8a8c6e51bc0375d857ce7
Abhishek Bose <abhishekbose87@gmail.com>
2015-09-21 09:27:55 +0530

    Add model specs for uniqueness and presence constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  validates :name, presence: true, uniqueness: true
+  validates :slug, uniqueness: true, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the uniqueness constraint" do
+    user = User.create(name: "Abhishek")
+    new_user = User.new(name: "Abhishek")
+    assert_not new_user.save
+    assert_equal new_user.errors.full_messages, ["Name has already been taken"]
+  end
+
+  test "the presence constraint" do
+    user = User.new(name: "")
+    assert_not user.valid?
+  end
 end
